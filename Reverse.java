690551dc49ef3ae5babd5a64da7515eb24ac5c20
noaaviran <154530417+noaaviran@users.noreply.github.com>
2023-12-31 14:09:25 +0200

    Add files via upload

--- Reverse.java
+++ Reverse.java
@@ -2,13 +2,20 @@ public class Reverse {
 	public static void main(String args []) {
 		 String s = (args[0]);
 		 int lastletter = (s.length ()-1);
-		 int middle = (s.length () / 2);
-		
+		 int length= s.length(); 
+		 int middle;
+		 
+		 if ( length % 2 == 0) {
+			  middle = (s.length () / 2 -1);
+		 } else {
+			  middle = (s.length () / 2 );
+		 }
+		 
           while ( lastletter >= 0) {			  
 		 System.out.print( s.charAt(lastletter));
 		  lastletter = lastletter - 1; }
 		 System.out.println(" " );
-		 System.out.println("the middle letter is " + s.charAt( middle-1));
+		 System.out.println("the middle letter is " + s.charAt( middle));
 
 
 		  }
