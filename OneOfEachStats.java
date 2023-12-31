b524e8e14ac97b55ddd3351ee1e701d2d51e3f8c
noaaviran <154530417+noaaviran@users.noreply.github.com>
2023-12-31 16:27:52 +0200

    Add files via upload

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -13,7 +13,57 @@ public class OneOfEachStats {
 		int T = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
-        Random generator = new Random(seed);  
+        Random generator = new Random(seed); 
+		
+		int kids=0;
+		int total=0 ;
+		int twoKids=0;
+		int threeKids=0;
+		int fourOrMore=0;
+		int count=0;
+		
+
+		for (int i=0; i<T; i++) {
+		boolean girl= true;
+		boolean boy= true;
+		
+		int numOfKids=0;
+		 while (girl || boy) {
+			 kids = 0 ;
+			 double rnd = generator.nextDouble();
+			  if (rnd == 1) {
+			  girl= false;
+			  kids++;
+			  }
+			  else { 
+				  
+				  boy= false;
+                  kids++;					  
+			  }
+			  total ++;
+			numOfKids++;
+		 }
+		switch(numOfKids) {
+			case 2:
+				twoKids++;
+				break;
+			case 3:
+				threeKids++;
+				break;
+			default:
+				fourOrMore++;
+				break;
+			}
+		}
+		 int check= Math.max(Math.max(twoKids, threeKids), fourOrMore);
+		 double avarage = (double) total / T;
+	     System.out.println("Avrage: " +avarage+ "children to get at least one of each gender.");
+		 System.out.println("Number of families with 2 children: "+ twoKids);
+		 System.out.println("Number of families with 3 children: " + threeKids);
+		 System.out.println("Number of families with 4 children: "+ fourOrMore);
+		 System.out.println("The most common number is " + check + ".");
+		 
+		
 		
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
